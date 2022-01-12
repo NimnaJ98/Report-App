885832b04d5b3d68a759b99e578a823cc57f61c3
Nimna <e1846608@bit.mrt.ac.lk>
2022-01-12 19:38:34 +0530

    report models added

--- src/reports/models.py
+++ src/reports/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from profiles.models import Profile
 
 # Create your models here.
+class Report(models.Model):
+    name = models.CharField(max_length=120)
+    #to store charts
+    image = models.ImageField(upload_to='reports', blank=True)
+    #to store thoughts based on charts
+    remarks = models.TextField()
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)    #foreign key from profiles model
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
+
+        
