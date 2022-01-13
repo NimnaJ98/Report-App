0cded42c7270037c7aa37c696cc46093b3a8174b
Nimna <e1846608@bit.mrt.ac.lk>
2022-01-13 09:03:43 +0530

    created home view

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#using a function view since later will be adding lot of logics to this
+def home_view(request):
+    return render(request, 'sales/main.html', {})
